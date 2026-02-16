03f590b7c8f29708961b3d00234a42d9359771c1
matthewyungcw-web <matthew.yungcw@gmail.com>
2026-02-16 17:59:23 +0800

    Update __init__.py

--- functions/ui/__init__.py
+++ functions/ui/__init__.py
@@ -1,1 +1,7 @@
+# Recommended content for functions/ui/__init__.py
+"""UI package for COMP2090SEF Library System"""
 
+from .user_gui import UserGUI, launch_gui
+
+__version__ = "1.0.0"
+__all__ = ['UserGUI', 'launch_gui']
