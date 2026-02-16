79f403a07e2c3d987393c40a09d23b03b9e96a9f
matthewyungcw-web <matthew.yungcw@gmail.com>
2026-02-16 18:07:09 +0800

    Update book.py

--- functions/models/book.py
+++ functions/models/book.py
@@ -1,1 +1,10 @@
+class Book:
+    def __init__(self, title, isbn, author="", available=True):
+        self.title = title
+        self.isbn = isbn
+        self.author = author
+        self.available = available
+    
+    def __str__(self):
+        return f"{self.title} by {self.author} (ISBN: {self.isbn})"
 
