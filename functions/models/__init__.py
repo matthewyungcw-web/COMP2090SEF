8b10e0cad02d7ec6254f771a388abfa7d0f98b12
matthewyungcw-web <matthew.yungcw@gmail.com>
2026-03-06 12:25:39 +0800

    Update __init__.py

--- functions/models/__init__.py
+++ functions/models/__init__.py
@@ -1,4 +1,4 @@
 # functions/models/__init__.py
 from .book import Book
 from .user import User
-__all__ = ['Book', 'User']  # Controls "from models import *"
+__all__ = ['Book', 'User']  #models import
